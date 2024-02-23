e3bbbe72132733df581d2ffe11963401160a85d5
Valerio Corda <85554215+VALDEV05@users.noreply.github.com>
2024-02-23 10:49:38 +0100

    custom

--- wp-content/themes/aurum/tpls/header-logo_custom.php
+++ wp-content/themes/aurum/tpls/header-logo_custom.php
@@ -1,2 +1,12 @@
 <!-- LOGO -->
-LOGO
+<?php
+$url_logo_egho =  get_field('logo-first-egho','options')["url"];
+$home_url = get_home_url();
+?>
+
+<div class="container-logo_egho">
+    <a href="<?php $home_url;?>">
+        <img src="<?php echo $url_logo_egho;?>" alt="">
+    </a>
+</div>
+<!-- /.container-logo_egho -->
